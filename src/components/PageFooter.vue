1069f88c33ee2380f1de0aab25fc470ef8eba1b7
jacky.li <jacky.li@accenture.com>
2021-07-07 11:06:06 +0800

    fix issue #40

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -2,22 +2,22 @@
 <section class="t-footer bg_2 js_footer">
   <div class="c-m" style="width:960px;margin:0 auto;">
 
-    <el-row>
+    <!-- <el-row>
       <el-col :span="6">
         <h4 class="t-h">Resources</h4>
-        <a class="t-a" target="_blank" href="http://tearust.com">Tearust</a>
+        <a class="t-a" target="_blank" href="http://tearust.com">Tearust</a> -->
         <!-- <a class="t-a" target="_blank" href="https://medium.com/@pushbar">Medium</a> -->
         <!-- <a class="t-a" target="_blank" href="https://github.com/tearust/tea-docs/discussions">Discussions</a> -->
         
-      </el-col>
+      <!-- </el-col>
       <el-col :span="12">
         <h4 class="t-h">Tools</h4>
         <a class="t-a" href="javascript:void(0)" @click="$router.push('/tools/calculate_staking_profit');">Staking收益计算器</a>
       </el-col>
-    </el-row>
+    </el-row> -->
 
-    <el-divider></el-divider>
-    <p style="margin:0 auto;text-align:center;font-size:16px;">Copyright © 2019-2020 <b>Gluon-Wallet</b> All Rights Reserved</p>
+    <!-- <el-divider></el-divider> -->
+    <p style="margin:0 auto;text-align:center;font-size:16px;">Copyright © 2019-2021 <b>TeaProject.org</b> All Rights Reserved</p>
   </div>
 </section>
 
