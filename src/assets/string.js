2313af695e729bca47c5d5b2bedb85470f174832
kevin.zhang.canada@gmail.com <kevin.zhang.canada@gmail.com>
2021-07-09 12:06:38 -0700

    fix text

--- src/assets/string.js
+++ src/assets/string.js
@@ -1,5 +1,5 @@
 export default {
-  UnstakingButtonDisabledReason: 'You can\'t unstaking the first slot.',
+  UnstakingButtonDisabledReason: 'You can\'t unstake the first slot unless you sell this Camellia.',
   FrozenSeed: 'Frozen seed',
   FreshSeed: 'Fresh seed',
   PutCmlToAuctionSore: 'Put cml to auction store',
