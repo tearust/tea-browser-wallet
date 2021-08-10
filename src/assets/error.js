953d20b3e3cbe246303da2e2fcdd3324a4361cb5
jacky.li <jacky.li@accenture.com>
2021-08-10 15:16:27 +0800

    update for issue #144

--- src/assets/error.js
+++ src/assets/error.js
@@ -81,4 +81,43 @@ export default {
   CmlInvalidStatusConversion: 'The current CML status cannot be converted to another status',
 
   OperationForbiddenWithCredit: 'Withdrawal not permitted while account has an active loan. First pay off any outstanding loans before withdrawing rewards.',
+
+  // genesis bank
+  /// Loan already exists that cannot be pawn again.
+  LoanAlreadyExists: 'LoanAlreadyExists',
+  /// The given asset id not exist in collateral store.
+  LoanNotExists: 'LoanNotExists',
+  /// Collateral not belongs to user.
+  InvalidBorrower: 'InvalidBorrower',
+  /// Loan in default
+  LoanInDefault: 'LoanInDefault',
+  /// User have not enough free balance to pay off loan.
+  InsufficientRepayBalance: 'InsufficientRepayBalance',
+  /// Close height should larger equal than current height.
+  InvalidCloseHeight: 'InvalidCloseHeight',
+  /// Only frozen seeds are allowed to be collateral
+  ShouldPawnFrozenSeed: 'ShouldPawnFrozenSeed',
+  /// Only genesis seeds are allowed to be collateral
+  ShouldPawnGenesisSeed: 'ShouldPawnGenesisSeed',
+  /// Collateral store is not empty and bank cannot shutdown.
+  CollateralStoreNotEmpty: 'CollateralStoreNotEmpty',
+  /// User collateral store not empty cannot shutdown.
+  UserCollateralStoreNotEmpty: 'UserCollateralStoreNotEmpty',
+  /// Loan id convert to cml id with invalid length.
+  ConvertToCmlIdLengthMismatch: 'ConvertToCmlIdLengthMismatch',
+  /// Con not apply loan after current height larger equal than the close height.
+  /// Close height is a preset block height that the Genesis Bank will stop operation
+  /// We have such a close time because Genesis bank is supposed to be temporary cold-start
+  /// helper. When newer Defi service tApps are ready, the Genesis Bank should be retired
+  CannotApplyLoanAfterClosed: 'CannotApplyLoanAfterClosed',
+
+  // genesis exchange
+  ExchangeInsufficientUSD: 'ExchangeInsufficientUSD',
+  ExchangeInsufficientTEA: 'ExchangeInsufficientTEA',
+  UserInsufficientUSD: 'UserInsufficientUSD',
+  UserInsufficientTEA: 'UserInsufficientTEA',
+  InvalidDepositAmount: 'InvalidDepositAmount',
+  InvalidTransferUSDAmount: 'InvalidTransferUSDAmount',
+  WithdrawAmountShouldNotBeZero: 'WithdrawAmountShouldNotBeZero',
+
 };
