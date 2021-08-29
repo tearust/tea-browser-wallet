61f25b5f1e0b10a353e76d35c06cd6c1fd324da3
geopor <gzed12@gmail.com>
2021-08-29 15:48:52 -0700

    Added error messages

--- src/assets/error.js
+++ src/assets/error.js
@@ -164,11 +164,11 @@ export default {
   NotAllowedNormalUserCreateTApp: 'You don\'t have enough permission to creat a TApp',
   OnlyTAppOwnerAllowedToExpense: 'Error: only TApp owner allowed to expense',
 
-  HostPerformanceAndMaxAllowedHostMustBePaired: 'HostPerformanceAndMaxAllowedHostMustBePaired',
-  PerformanceValueShouldNotBeZero: 'PerformanceValueShouldNotBeZero',
-  MaxAllowedHostShouldNotBeZero: 'MaxAllowedHostShouldNotBeZero',
-  TAppNotSupportToHost: 'TAppNotSupportToHost',
-  TAppHostMachineIsFull: 'TAppHostMachineIsFull',
-  CmlMachineIsFullLoad: 'CmlMachineIsFullLoad',
-  CmlNotHostTheTApp: 'CmlNotHostTheTApp',
+  HostPerformanceAndMaxAllowedHostMustBePaired: 'Host performance and Max allowed hosts should both have a value if both aren\'t 0',
+  PerformanceValueShouldNotBeZero: 'Performance value shouldnot be 0',
+  MaxAllowedHostShouldNotBeZero: 'Max allowed hosts should not be 0',
+  TAppNotSupportToHost: 'This TApp no longer supports being hosted',
+  TAppHostMachineIsFull: 'This TApp has reached its max desired hosts',
+  CmlMachineIsFullLoad: 'CML machine is already at full load',
+  CmlNotHostTheTApp: 'CML machine is not hosting this TApp',
 };
