db9e4ceb31df3d31fcbc833785345745c74bdced
geopor <86096370+geopor@users.noreply.github.com>
2022-01-18 19:22:58 -0800

    Updated announcement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,22 @@
 
   <el-tabs type="border-card">
     <el-tab-pane label="Investors">
+    
+<strong style="font-size:18px;">Epoch 8 Officially Starts Jan 21, 2022</strong>
+<p>
+The chain will restart January 18 around 9pm PST. Do not be alarmed if you see many miners offline during the transition. Starting January 19, we will begin reissuing contest CML seeds. Once they're in your wallet, you may plant the CML into your mining machines and start bringing them back online ahead of the official January 21 epoch 8 start date.
+</p>
+
+<p>
+  A snapshot of all wallets will be taken right before the chain is restarted. The snapshot will ensure users have the same amount of assets in the new epoch and will include the following:
+</p>
+<ul>
+  <li>All users will have their TApp tokens liquidated for their underlying value according to the bonding curve (a fair distribution will be used where all owners will sell at the same price).</li>
+  <li>All TEA in a user's wallet will be brought over from epoch 7.1 to epoch 8.</li>
+  <li>Any unused frozen CML seeds that a user has during epoch 7.1 will be reissued as CML coupons in epoch 8.</li>
+  <li>For CML mining nodes that are currently running in epoch 7.1, no matter how much life span they have left, each of these will also be reissued as CML coupons in epoch 8.</li>
+</ul>
+
 <strong style="display:block; font-size: 18px;">Investors</strong>
 <p>
 Investors have two ways of participating in the TEA Project ecosystem: CML and TApp token investing.
@@ -32,9 +48,13 @@ Investors purchasing TApp tokens and purchasing or staking CML will incur transa
     <el-tab-pane label="Miners">
 
 
-<strong style="font-size:18px;">Epoch 8 Starts Jan 21, 2022</strong>
+<strong style="font-size:18px;">Epoch 8 Officially Starts Jan 21, 2022</strong>
 <p>
-  A snapshot of all wallets will be taken right before the epoch 8 launch. The snapshot will ensure users have the same amount of assets in the new epoch and will include the following:
+The chain will restart January 18 around 9pm PST. Do not be alarmed if you see many miners offline during the transition. Starting January 19, we will begin reissuing contest CML seeds. Once they're in your wallet, you may plant the CML into your mining machines and start bringing them back online ahead of the official January 21 epoch 8 start date.
+</p>
+
+<p>
+  A snapshot of all wallets will be taken right before the chain is restarted. The snapshot will ensure users have the same amount of assets in the new epoch and will include the following:
 </p>
 <ul>
   <li>All users will have their TApp tokens liquidated for their underlying value according to the bonding curve (a fair distribution will be used where all owners will sell at the same price).</li>
